9387355db961a6de7087de0d47c28f3a1f55c2be
InjaeJayLee <inwo9091@gmail.com>
2022-10-24 22:22:48 +0900

    set_plot_labels

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import seaborn as sns
+import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
-from typing import Tuple
+from typing import Tuple, List
 import pandas as pd
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 
@@ -45,6 +47,14 @@ def get_new_df_with_onehot_encoding(df: pd.DataFrame, col_name: str, is_train_da
 
 
 ########## Statistics #############
+def get_vif(df: pd.DataFrame):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    target_df = df.select_dtypes(include=numerics)
+    vif = pd.DataFrame()
+    vif["VIF Factor"] = [variance_inflation_factor(target_df.values, i) for i in range(target_df.shape[1])]
+    vif["Feature Name"] = target_df.columns
+    vif = vif.sort_values("VIF Factor").reset_index(drop=True)
+    return vif
 
 
 ###################################
@@ -58,7 +68,19 @@ def draw_heatmap(df: pd.DataFrame):
             df: a data frame to draw a heatmap of
     """
     plt.figure(figsize=(30, 30))
-    sns.set(font_scale=2.5)
-    sns.heatmap(df.corr(), annot=True, annot_kws={"size": 24})
+    sns.set(font_scale=2.2)
+    sns.heatmap(df.corr(), cmap='vlag', annot=True, annot_kws={"size": 24})
     plt.show()
+
+def set_plot_labels(ax, title: str, xlab: str="", ylab: str="", legend:List=None, xticks=None, yticks=None):
+    ax.set_xlabel(xlab, size=25)
+    ax.set_ylabel(ylab, size=25)
+    plt.title(title, fontsize=35)
+    if legend:
+        plt.legend(labels=legend, fontsize=20)
+    if xticks:
+        plt.xticks(xticks, size=20)
+    if yticks:
+        plt.yticks(yticks, size=20)
+    return ax
 ###################################
